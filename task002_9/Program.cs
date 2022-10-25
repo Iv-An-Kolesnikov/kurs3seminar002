9fb0c4eb7e9c8a338bf359244f9f932703761d41
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-25 15:14:10 +0300

    add task002_9

---
+++ task002_9/Program.cs
@@ -0,0 +1,19 @@
+﻿//9. Напишите программу, которая выводит случайное число 
+//из отрезка [10; 99] и показывает наибольшую цифру числа.
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+Console.Clear();
+//Console.Write();
+int num = new Random().Next(10, 100);
+Console.Write(num + " ");
+int num1 = num % 10;
+int num2 = num / 10;
+if (num1 > num2)
+{
+    Console.Write(num1);
+}
+else
+{
+    Console.Write(num2);
+}
