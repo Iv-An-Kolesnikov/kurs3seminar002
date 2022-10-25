e18435ff4cf1ad09c1dbbe56c5c13060baa29c34
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-25 17:58:12 +0300

    task002_16 2

--- task002_16/Program.cs
+++ task002_16/Program.cs
@@ -4,26 +4,37 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8, 9 -> нет
+//=================================================================================
+
+// Console.Clear();
+// int num1;
+// int num2;
+// int max;
+// int result;
+// Console.Write("Введите первое число: ");
+// num1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// num2 = Convert.ToInt32(Console.ReadLine());
+// if (num1 > num2) 
+// {
+//     max = num1;
+//     result = num2 * num2;
+//     if (result == max) Console.Write("Да");
+//     else Console.Write("Нет");
+// }
+// else 
+// {
+//     max = num2;
+//     result = num1 * num1;
+//     if (result == max) Console.Write("Да");
+//     else Console.Write("Нет");
+// }
+//==================================================================================
+
 Console.Clear();
-int num1;
-int num2;
-int max;
-int result;
 Console.Write("Введите первое число: ");
-num1 = Convert.ToInt32(Console.ReadLine());
+int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
-num2 = Convert.ToInt32(Console.ReadLine());
-if (num1 > num2) 
-{
-    max = num1;
-    result = num2 * num2;
-    if (result == max) Console.Write("Да");
-    else Console.Write("Нет");
-}
-else 
-{
-    max = num2;
-    result = num1 * num1;
-    if (result == max) Console.Write("Да");
-    else Console.Write("Нет");
-}
+int num2 = Convert.ToInt32(Console.ReadLine());
+if ((num1 * num1 == num2) || (num2 * num2 == num1)) Console.Write("Да");
+else Console.Write("Нет");
