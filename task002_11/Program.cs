f59e1cc852b56929f0e7efe2d9a090d585b8cbec
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-25 16:56:23 +0300

    add task002_11

---
+++ task002_11/Program.cs
@@ -0,0 +1,12 @@
+﻿//11. Напишите программу, которая выводит случайное трехзначное число
+//и удаляет вторую цифру этого числа.
+
+Console.Clear();
+int num = new Random().Next(100, 1000);
+int pervoe = num / 100;
+int dvaPervih = num / 10;
+int tretie = num - dvaPervih*10;
+
+Console.WriteLine(num);
+Console.Write(pervoe);
+Console.WriteLine(tretie);
