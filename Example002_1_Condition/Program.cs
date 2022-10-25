fdb696ea72ceb37159788e4f020d73a23c43b7dc
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-25 15:10:15 +0300

    add Example 002_1 Condition

---
+++ Example002_1_Condition/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задание условий
+Console.Clear();
+Console.Write("Введите число: ");
+int num1 = 5;
+int num2 = 2;
+if ((num1 == 5) && (num2 == 2))
+{
+    Console.Write("Да");
+}
+else
+{
+    Console.Write("Нет");
+}
