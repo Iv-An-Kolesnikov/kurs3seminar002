159c73711f535dba4e35734f10cfa9a2762595a1
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-25 15:13:23 +0300

    add Example 002_3 While

---
+++ Example002_3_While/Program.cs
@@ -0,0 +1,16 @@
+﻿//
+// Задание циклов
+Console.Clear();
+//Console.Write("Введите число: ");
+int num1 = 5;
+
+// while (num1 < 10)
+// {
+//     Console.Write("Да");
+//     num1 += 1;
+// }
+
+for (int i = 5; i < 55; i = i + 5)
+{
+    Console.Write(i + " ");
+}
