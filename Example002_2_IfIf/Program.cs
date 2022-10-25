8f32197a64f9d50956c8f31bb04a27750447e73c
Iv-An-Kolesnikov <i_a_kolesnikov@list.ru>
2022-10-25 15:11:52 +0300

    add Example002_2 IfIf

---
+++ Example002_2_IfIf/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задание каскадных условий
+Console.Clear();
+Console.Write("Введите число: ");
+int num1 = 5;
+int num2 = 2;
+int num3 = 3;
+
+if (num1 == 5)
+{
+    Console.Write("Да1");
+}
+if (num2 == 2)
+{
+     Console.Write("Да2");
+}
+if (num3 == 3)
+{
+     Console.Write("Да3");
+}
+else
+{
+    Console.Write("Нет");
+}
